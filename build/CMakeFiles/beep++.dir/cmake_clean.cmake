f840680a887f2a0352bd154d574d586f87ba3112
DameChocolateYa <samujf009@gmail.com>
2025-04-09 21:45:41 +0200

    First commit

---
+++ build/CMakeFiles/beep++.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/beep++.dir/link.d"
+  "CMakeFiles/beep++.dir/src/main.cpp.o"
+  "CMakeFiles/beep++.dir/src/main.cpp.o.d"
+  "beep++"
+  "beep++.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/beep++.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
