a6d9a5e71003ae8c4895ab63340cf1ded8e3ccf6
DameChocolateYa <samujf009@gmail.com>
2025-04-24 20:40:12 +0200

    Im regret of using rust

---
+++ build/CMakeFiles/beepc.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for beepc.
+# This may be replaced when dependencies are built.
--- target/debug/deps/beeplang-3636eff463f81202.d
+++ target/debug/deps/beeplang-3636eff463f81202.d
@@ -1,6 +1,7 @@
-/home/samuel/dev/cpp/compiler/target/debug/deps/beeplang-3636eff463f81202: src/main.rs src/lexer.rs
+/home/samuel/dev/cpp/compiler/target/debug/deps/beeplang-3636eff463f81202: src/main.rs src/lexer.rs src/parser.rs
 
-/home/samuel/dev/cpp/compiler/target/debug/deps/beeplang-3636eff463f81202.d: src/main.rs src/lexer.rs
+/home/samuel/dev/cpp/compiler/target/debug/deps/beeplang-3636eff463f81202.d: src/main.rs src/lexer.rs src/parser.rs
 
 src/main.rs:
 src/lexer.rs:
+src/parser.rs:
