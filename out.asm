e9e16b697743e8d2d1dc44d78f008dc9b33d166c
DameChocolateYa <samujf009@gmail.com>
2025-04-28 20:13:14 +0000

    Bah

--- out.asm
+++ out.asm
@@ -1,7 +1,7 @@
 global main
 section .text
   extern free
-pr:
+imprimir:
   push rbp
   mov rbp, rsp
   mov rdi, 2
@@ -22,40 +22,23 @@ pr:
   mov rsp, rbp
   pop rbp
   ret
-sc:
-  push rbp
-  mov rbp, rsp
-  mov rdi, 2
-  mov rdx, 0
-  lea rsi, [ rel str_1]
-  push rsi
-  call print
-
-  call scani
-
-  push rax
-  mov rsi, QWORD [rsp + 0]
-  push rsi
-  push rax
-  call pr
-  mov rsp, rbp
-  pop rbp
-  ret
 start:
   push rbp
   mov rbp, rsp
-  call sc
+  lea rax, [ rel str_1]
+  push rax
+  push rax
+  call imprimir
   mov rsp, rbp
   pop rbp
   ret
 main:
   extern print
-  extern scani
   call start
     mov rax, 60
   mov rdi, 0
   syscall
 
 section .rodata
-str_0: db "EL INPUT ES: ", 0
-str_1: db "PROMPT: ", 0
+str_0: db "", 0
+str_1: db "AH", 0
