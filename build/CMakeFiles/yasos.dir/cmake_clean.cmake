4b5f3ab1d9c8b22afebfcbc78b7c07cc80ea7a17
DameChocolateYa <samujf009@gmail.com>
2026-03-17 19:02:38 +0100

    Added images to the repository

---
+++ build/CMakeFiles/yasos.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yasos.dir/link.d"
+  "CMakeFiles/yasos.dir/src/error.cc.o"
+  "CMakeFiles/yasos.dir/src/error.cc.o.d"
+  "CMakeFiles/yasos.dir/src/generation.cc.o"
+  "CMakeFiles/yasos.dir/src/generation.cc.o.d"
+  "CMakeFiles/yasos.dir/src/lexer.cc.o"
+  "CMakeFiles/yasos.dir/src/lexer.cc.o.d"
+  "CMakeFiles/yasos.dir/src/main.cc.o"
+  "CMakeFiles/yasos.dir/src/main.cc.o.d"
+  "CMakeFiles/yasos.dir/src/parser.cc.o"
+  "CMakeFiles/yasos.dir/src/parser.cc.o.d"
+  "yasos"
+  "yasos.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/yasos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
